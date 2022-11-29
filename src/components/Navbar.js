b568dc98a0a4e721c6221c080470f05a52ebf155
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-29 16:36:15 +0100

    Applying the material-ui and using flexbox to center the items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,48 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color:black;
-`
+`;
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`;
+
+const Language = styled.span`
+   font-size: 14px;
+   cursor: pointer;
+`;
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+`;
+
+const Center = styled.div`flex: 1;`;
+const Right = styled.div`flex: 1;`;
 
 const Navbar = () => {
     return (
         <Container>
-            Navbar
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        input
+                        <Search />
+                    </SearchContainer>
+                </Left>
+                <Center>center</Center>
+                <Right>right</Right>
+            </Wrapper>
         </Container>
     )
 }
