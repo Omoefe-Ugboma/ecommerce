012193b7d0430a3a2fdddb3025b69c7a0c6dcd17
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-29 17:06:11 +0100

    Working on the search button with material-ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
+    align-items: center;
     justify-content: space-between;
 `;
 const Left = styled.div`
@@ -21,10 +22,16 @@ const Language = styled.span`
    cursor: pointer;
 `;
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
+    border: 0.5px solid lightgray;
     display: flex;
     align-items: center;
+    margin-left: 25px;
+    padding: 5px;
 `;
+
+const Input = styled.input`
+    border:none;
+`
 
 const Center = styled.div`flex: 1;`;
 const Right = styled.div`flex: 1;`;
@@ -36,7 +43,7 @@ const Navbar = () => {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        input
+                        <Input />
                         <Search />
                     </SearchContainer>
                 </Left>
