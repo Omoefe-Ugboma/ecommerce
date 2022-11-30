c3be0fa08318d075fab0883d2b3686f4987dccdf
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-30 12:00:56 +0100

    Continuation of the Slider section

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,7 +5,7 @@ const Container = styled.div`
    width: 100%;
    height: 100vh;
    display: flex;
-   /* background-color: coral; */
+   background-color: coral;
    position: relative;
 `;
 
@@ -27,13 +27,53 @@ const Arrow = styled.div`
     opacity: 0.5;
 `;
 
+const Wrapper = styled.div`
+   height: 100%;
+`;
+
+const Slide = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+align-items: center;
+`;
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`;
+
+const Image = styled.img`
+height: 80%;
+`;
+
+const InfoContainer = styled.div`
+flex: 1;
+padding: 50px;
+`;
+
+const Title = styled.h1``
+const Desc = styled.p``
+const Button = styled.button``
+
 function Slider() {
     return (
         <Container>
             <Arrow direction="left">
                 <ArrowLeftOutlined />
             </Arrow>
-
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="https://play-lh.googleusercontent.com/xpx5LFTz1DYMFze9HZNl9lahGtT2jhNqT6SLiWxMN-XKfWlTFiH3Vfq_dBntv1M9L9c" />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>SUMMER SALE</Title>
+                        <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+                        <Button></Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
             <Arrow direction="right">
                 <ArrowRightOutlined />
             </Arrow>
