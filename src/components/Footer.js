55d059ab1bdf07020668300b548ddbe5bd77e12c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-01 15:14:09 +0100

    working on the footer information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,117 @@
-import React from 'react'
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+display: flex;
+`
+const Left = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
+`
+const Logo = styled.h1``;
+const Desc = styled.p`
+margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+display: flex;
+`;
+const SocialIcon = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color: white;
+background-color: #${props => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+margin-right: 20px;
+`;
+
+const Center = styled.div`
+flex: 1;
+padding: 20px;
+`
+const Title = styled.h3`
+margin-bottom: 30px;
+`
+const List = styled.ul`
+margin: 0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`
+const ListItem = styled.li`
+width: 50%;
+`
+
+const Right = styled.div`
+flex: 1;
+padding: 20px;
+`
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`;
+
+const Payment = styled.div`
+
+`
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <Container>
+            <Left>
+                <Logo>Fashion</Logo>
+                <Desc>
+                    n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room /> 454 Dixs Path, South Tobmix 434
+                </ContactItem>
+                <ContactItem>
+                    <Phone /> +2349023454332
+                </ContactItem>
+                <ContactItem>
+                    <MailOutline /> contact@fashion.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
     )
 }
 
