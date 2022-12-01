f53dca362e6edbc70495231c7337c84992435e04
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-01 09:33:45 +0100

    Newsletter section styling in progress

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -2,12 +2,39 @@ import { Send } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
-const Title = styled.h1``
-const Desc = styled.div``
-const InputContainer = styled.div``
-const Input = styled.input``
-const Button = styled.button``
+const Container = styled.div`
+height: 60vh;
+background-color: #fcf5f5;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+const Title = styled.h1`
+font-size: 70px;
+margin-bottom: 20px;
+`
+const Desc = styled.div`
+font-size: 24px;
+font-weight: 300;
+margin-bottom: 20px;
+`
+const InputContainer = styled.div`
+width: 50%;
+height: 40px;
+background-color: white;
+display: flex;
+justify-content: space-between;
+border: 1px solid lightgray;
+`
+const Input = styled.input`
+border: none;
+flex: 8;
+padding-left: 20px;
+`
+const Button = styled.button`
+flex: 1;
+`
 
 const Newsletter = () => {
     return (
