dd6e730257deff443ed7f8451fcb928193af520c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-01 08:42:49 +0100

    front product section styling in progress

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,9 +4,13 @@ import styled from 'styled-components'
 const Container = styled.div`
 flex: 1;
 margin: 5px;
+min-width: 280px;
+height: 350px;
 `
 const Circle = styled.div``
-const Image = styled.img``
+const Image = styled.img`
+height: 75%;
+`
 const Info = styled.div``
 const Icon = styled.div``
 
