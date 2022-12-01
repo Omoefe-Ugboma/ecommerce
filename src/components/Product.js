7a79c2249b77312d70a538ae4f2df03f635d4555
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-01 08:27:08 +0100

    Product section updating progress

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,32 @@
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+flex: 1;
+margin: 5px;
+`
+const Circle = styled.div``
+const Image = styled.img``
+const Info = styled.div``
+const Icon = styled.div``
 
-const Product = () => {
+const Product = ({ item }) => {
     return (
-        <Container>Product</Container>
+        <Container>
+            <Circle />
+            <Image src={item.img} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined />
+                </Icon>
+                <Icon>
+                    <SearchOutlined />
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined />
+                </Icon>
+            </Info>
+        </Container>
     )
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import styled from "styled-components"
+import { popularProducts } from "../data"
+import Product from "./Product"
+
+const Container = styled.div`
+padding: 20px;
+display: flex;
+`
 
 const Products = () => {
     return (
-        <div>Products</div>
+        <Container>
+            {popularProducts.map((item) => (
+                <Product item={item} key={item.id} />
+            ))}
+        </Container>
     )
 }
 
