78d2adf8512e905118a51c82bd08ff130cf8fa0d
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-01 08:03:51 +0100

    Font Product in progress

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Product = () => {
+    return (
+        <Container>Product</Container>
+    )
+}
+
+export default Product
