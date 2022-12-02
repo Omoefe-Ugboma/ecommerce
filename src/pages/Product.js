3a0ca3dbd4e02e4a47395f35383f4a40538fd5ca
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-02 12:06:09 +0100

    Product section in progress

---
+++ src/pages/Product.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+
+const Container = styled.div``
+
+const Product = () => {
+    return (
+        <Container>Product</Container>
+    )
+}
+
+export default Product
