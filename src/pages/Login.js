a8f228814af8cfb4b621350c76c2737a576b84fd
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-02 20:39:38 +0100

    Login section in progress as well

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const Login = () => {
+    return (
+        <Container>Login</Container>
+    )
+}
+
+export default Login
