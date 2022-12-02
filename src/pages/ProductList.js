c87ed642a3c641ec9cf03a78750542f76b8565c3
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-02 11:56:11 +0100

    completed the product list page

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -17,7 +17,13 @@ margin: 20px;
 const FilterText = styled.span`
 font-size: 20px;
 font-weight: 600;
+margin-right: 20px;
 `
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;
+`
+const Option = styled.option``
 
 const ProductList = () => {
     return (
@@ -26,8 +32,33 @@ const ProductList = () => {
             <Announcement />
             <Title>Dresses</Title>
             <FilterContainer>
-                <Filter>filter</Filter>
-                <Filter>filter2</Filter>
+                <Filter>
+                    <FilterText>Filter Products:</FilterText>
+                    <Select>
+                        <Option disabled selected>Color</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Products:</FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
+                </Filter>
             </FilterContainer>
             <Products />
             <Newsletter />
