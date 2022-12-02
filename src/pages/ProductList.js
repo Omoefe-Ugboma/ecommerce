f2a1bf88587f31dff1321042570a6099b62da4c9
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-02 09:53:04 +0100

    Including all the product list, newsletter, and footer

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,10 +1,38 @@
 import styled from "styled-components"
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 const Container = styled.div``
+const Title = styled.h1``
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Filter = styled.div`
+margin: 20px;
+`
+const FilterText = styled.span`
+font-size: 20px;
+font-weight: 600;
+`
 
 const ProductList = () => {
     return (
-        <Container>ProductList</Container>
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Title>Dresses</Title>
+            <FilterContainer>
+                <Filter>filter</Filter>
+                <Filter>filter2</Filter>
+            </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
+        </Container>
     )
 }
 
