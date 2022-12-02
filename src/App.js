866480ba508a83684ff59d2c50e22a001c43c077
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-02 12:40:21 +0100

    style product page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Product from './pages/Product';
 // import Home from "./pages/Home"
-import ProductList from './pages/ProductList';
+// import ProductList from './pages/ProductList';
 
 const App = () => {
   return (
     <div className="App">
 
       {/* <Home /> */}
-      <ProductList />
+      <Product />
 
     </div>
   );
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,53 @@
 import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
 
 
 const Container = styled.div``
+const Wrapper = styled.div`
+padding: 50px;
+display: flex;
+`
+const ImgContainer = styled.div`
+flex: 1;
+`
+const Image = styled.img`
+width: 100%;
+height: 90vh;
+object-fit: cover;
+`
+const InfoContainer = styled.div`
+flex: 1;
+padding: 0px 50px;
+`
+const Title = styled.h1`
+font-weight: 200;
+`
+const Desc = styled.p``
+const Price = styled.span``
 
 const Product = () => {
     return (
-        <Container>Product</Container>
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://www.reead.com/en/wp-content/uploads/2020/02/Skinny-Jeans-men.webp" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Denim Jumpsuit</Title>
+                    <Desc>
+                        Discover how you can elevate the way you work by combining webinar, chat, meetings, and phone solutions into one seamless platform.
+                    </Desc>
+                    <Price>$ 20</Price>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter />
+            <Footer />
+        </Container>
     )
 }
 
