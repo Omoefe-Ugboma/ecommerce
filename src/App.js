8ae09fdd6d650e6c320fa759a85d5306efdcdfc1
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-03 19:04:33 +0100

    UI design completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // import './App.css';
 // import Login from './pages/Login';
 // import Register from './pages/Register';
-import Product from './pages/Product';
+// import Product from './pages/Product';
 // import Home from "./pages/Home"
 // import ProductList from './pages/ProductList';
 
-// import Cart from './pages/Cart';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
     <div className="App">
 
       {/* <Home /> */}
-      <Product />
+      {/* <Product /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      {/* <Cart /> */}
+      <Cart />
       {/* <ProductList /> */}
 
     </div>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,10 +3,12 @@ import styled from "styled-components"
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
+import { mobile } from "../responsive";
 
 const Container = styled.div``
 const Wrapper = styled.div`
 padding: 20px;
+${mobile({ padding: "10px" })}
 `
 const Title = styled.div`
 font-weight: 300;
@@ -27,7 +29,9 @@ border: ${(props) => props.type === "filled" && "none"};
 background-color: ${(props) => props.type === "filled" ? "black" : "transparent"};
 color:${(props) => props.type === "filled" && "white"};
 `
-const TopTexts = styled.div``
+const TopTexts = styled.div`
+${mobile({ display: "none" })}
+`
 const TopText = styled.span`
 text-decoration: underline;
 cursor: pointer;
@@ -37,6 +41,7 @@ margin: 0px 10px;
 const Bottom = styled.div`
 display: flex;
 justify-content: space-between;
+${mobile({ flexDirection: "column" })}
 `
 const Info = styled.div`
 flex: 3;
@@ -44,6 +49,7 @@ flex: 3;
 const Product = styled.div`
 display: flex;
 justify-content: space-between;
+${mobile({ flexDirection: "column" })}
 `
 const ProductDetail = styled.div`
 flex: 2;
@@ -83,10 +89,12 @@ margin-bottom: 20px;
 const ProductAmount = styled.div`
 font-size: 24px;
 margin: 5px;
+${mobile({ margin: "5px 15px" })}
 `
 const ProductPrice = styled.div`
 font-size: 30px;
 font-weight: 200;
+${mobile({ marginBottom: "20px" })}
 `
 
 const Hr = styled.hr`
