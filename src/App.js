d4833f372a4c906a993cfad94d36dda6a40e46f5
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-03 08:52:19 +0100

    Login screen section in progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Register from './pages/Register';
+import Login from './pages/Login';
+// import Register from './pages/Register';
 // import Product from './pages/Product';
 // import Home from "./pages/Home"
 // import ProductList from './pages/ProductList';
@@ -10,7 +11,8 @@ const App = () => {
 
       {/* <Home /> */}
       {/* <Product /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login />
 
     </div>
   );
