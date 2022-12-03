be371c7d42b20474d0332262e70a8ef44047bad2
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-03 10:15:31 +0100

    product price detials and showing all the attribbutes of the product

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -33,7 +33,42 @@ cursor: pointer;
 margin: 0px 10px;
 `
 
-const Bottom = styled.div``
+const Bottom = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Info = styled.div`
+flex: 3;
+`
+const Product = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const ProductDetail = styled.div`
+flex: 2;
+display: flex;
+`
+const Image = styled.img`
+width: 200px;
+`
+const Details = styled.div`
+padding: 20px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+`
+const ProductName = styled.span``
+const ProductId = styled.span``
+const ProductColor = styled.div``
+const ProductSize = styled.span``
+const PriceDetail = styled.div`
+flex: 1;
+`
+
+
+const Summary = styled.div`
+flex: 1;
+`
 
 const Cart = () => {
     return (
@@ -52,7 +87,25 @@ const Cart = () => {
 
                     <TopButton type="filled">CHECKOUT NOW</TopButton>
                 </Top>
-                <Bottom></Bottom>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://images.unsplash.com/photo-1560343090-f0409e92791a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" />
+                                <Details>
+                                    <ProductName> <b>Product:</b> STRONG LEDDER SHOES</ProductName>
+                                    <ProductId> <b>ID:</b> 94323432234</ProductId>
+                                    <ProductColor />
+                                    <ProductSize> <b>Size:</b> 37.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                price
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
             </Wrapper>
             <Footer />
 
