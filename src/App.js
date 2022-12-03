c70cdeb5e5d980bbcf0045d241f4ec750f756a09
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-12-03 09:36:47 +0100

    Cart design still in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import Login from './pages/Login';
+// import './App.css';
+// import Login from './pages/Login';
 // import Register from './pages/Register';
 // import Product from './pages/Product';
 // import Home from "./pages/Home"
 // import ProductList from './pages/ProductList';
+
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
@@ -12,7 +14,8 @@ const App = () => {
       {/* <Home /> */}
       {/* <Product /> */}
       {/* <Register /> */}
-      <Login />
+      {/* <Login /> */}
+      <Cart />
 
     </div>
   );
